988709e446ef16775572a869315f8346fe3b48c7
Ryuuji73Takasu <Raihanul.Sourav@student.hpi.uni-potsdam.de>
2025-01-24 11:12:17 +0100

    causal query working

--- Application/wise-dag-backend/server.js
+++ Application/wise-dag-backend/server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const { driver } = require('./db/neo4j');
 const graphRoutes = require('./routes/graphRoutes');
 const iterationLevelRoutes = require('./routes/iterationLevelRoutes');
+const causalQueryRoutes = require('./routes/causalQueryRoutes');
+
 
 const app = express();
 const PORT = 3001;
@@ -15,6 +17,7 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api', graphRoutes);
 app.use('/api', iterationLevelRoutes);
+app.use('/api', causalQueryRoutes);
 
 // Close Neo4j connection when server stops
 process.on('SIGINT', async () => {
