fd974d06687cc4dc1a3f5e75860e4e9867c675d3
Leo <leonhardfriedrich@gmail.com>
2025-02-05 10:12:56 +0100

    fixed DropdownWithSearch small w causing to not work

--- Application/wise-dag-app/src/components/ResearchPage/ResearchPage.jsx
+++ Application/wise-dag-app/src/components/ResearchPage/ResearchPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import DropdownWithSearch from "../DropdownWithSearch/index";
+import DropdownWithSearch from '../DropdownWithSearch';
 import logo from "../../assets/logo.png";
 
 const fetchConcepts = async (searchTerm) => {
