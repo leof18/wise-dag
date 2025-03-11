7d0973ea0bb3301760de39d7e75fcf795d16fccf
JoshCai <ChunJie.Josh.Cai@Gmail.com>
2025-03-11 02:27:34 +0100

    Change Port

--- Application/wise-dag-backend/server.js
+++ Application/wise-dag-backend/server.js
@@ -10,7 +10,11 @@ const dagittyInputRoute = require("./routes/dagittyInput");
 const hierarchyQuery = require("./routes/hierarchyQuery");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Render!");
+});
 
 // Middleware
 app.use(cors());
@@ -31,6 +35,6 @@ process.on('SIGINT', async () => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
