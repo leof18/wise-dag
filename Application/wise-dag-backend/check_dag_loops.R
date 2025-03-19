3b0b6a6675239a76ecfc32086979bb5123b7a453
JoshCai <ChunJie.Josh.Cai@Gmail.com>
2025-03-19 23:30:11 +0800

    Fetch R update

--- Application/wise-dag-backend/check_dag_loops.R
+++ Application/wise-dag-backend/check_dag_loops.R
@@ -10,7 +10,11 @@ suppressWarnings(library(dagitty))
 args <- commandArgs(trailingOnly = TRUE)
 dag_file <- args[1]  # First argument is the DAG file path
 lines <- readLines(dag_file)  # Read DAG
-dag_string <- paste(lines, collapse = "")
+
+# Remove lines containing _t3, _t4, ... _tN (where N > 2)
+filtered_lines <- grep("_T[3-9]|_T[1-9][0-9]", lines, invert = TRUE, value = TRUE)
+
+dag_string <- paste(filtered_lines, collapse = "")
 dag <- dagitty(dag_string)
 
 if (isAcyclic(dag)) {
